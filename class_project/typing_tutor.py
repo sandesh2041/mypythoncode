9320aca7f316a6f9be1732fb7f3ef877b8625ae2
sandesh2041 <sandesh2041@users.noreply.github.com>
2022-08-19 07:24:17 +0000

    Final changes for project

--- class_project/typing_tutor.py
+++ class_project/typing_tutor.py
@@ -2,37 +2,43 @@
 """ Python Project | Sam Maskey
     This project will provide your tying speed and accuracy """
 
-
 import json
 import time
 import os
 import random
 import requests
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_file_string():
+    """Function extracting single random string from file"""
     filename = 'strings_easy.txt'
     print(f"Getting string from '{filename}'")
-    time.sleep(2)
-    file_open = open(filename).read()
-    strings = file_open.split('\n')
-    string = random.choice(strings)
-    return string
+    time.sleep(1)
+    os.system('clear')
+    with open(filename, encoding="utf-8") as file_open:
+        file_read = file_open.read()
+        strings = file_read.split('\n')
+        string = random.choice(strings)
+        return string
 
 
 def get_api_string():
-    url = "https://type.fit/api/quotes"
+    """Function extracting single random quote from API"""
+    url = "http://quotes.stormconsultancy.co.uk/quotes.json"
     print(f"Getting quote from '{url}'")
-    time.sleep(2)
+    time.sleep(1)
+    os.system('clear')
     response = requests.get(url)
     converted_response = json.loads(response.text)
     i = random.randint(1, len(converted_response))
-    string = converted_response[i]['text']
+    string = converted_response[i]['quote']
     return string
 
 
-# Calculating Character Accuracy
 def char_accuracy_calc(text_displayed, text_typed):
+    """Function calculating character accuracy"""
     count = 0
     for index, character in enumerate(text_typed):
         try:
@@ -45,8 +51,8 @@ def char_accuracy_calc(text_displayed, text_typed):
     return accuracy
 
 
-# Calculating Word Accuracy
 def word_accuracy_calc(text_displayed, text_typed):
+    """Function calculating word accuracy"""
     count = 0
     words_displayed = text_displayed.split()
     words_typed = text_typed.split()
@@ -62,68 +68,127 @@ def word_accuracy_calc(text_displayed, text_typed):
     return accuracy
 
 
+def chart(char_acc, word_acc, speed):
+    """Function creating bar chart"""
+    length = len(char_acc)
+    # Label Location
+    xaxis = np.arange(length) + 1
+    # Width of the bar
+    width = 0.35  # the width of the bars
+
+    fig, axes = plt.subplots()
+    rects1 = axes.bar(xaxis - width / 2, char_acc, width, label='Character Accuracy')
+    rects2 = axes.bar(xaxis + width / 2, word_acc, width, label='Word Accuracy')
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    axes.set_ylabel('Accuracy in %')
+    axes.set_title('Typing Speed and Accuracy')
+    axes.set_xticks(xaxis)
+    axes.legend()
+
+    axes.bar_label(rects1, padding=3)
+    axes.bar_label(rects2, padding=3)
+
+    axes2 = axes.twinx()
+    axes2.set_ylabel('Speed in wpm')
+    axes2.plot(xaxis, speed, 'o-', color='crimson', linewidth=2, label='Speed')
+    axes2.legend(loc="upper left")
+    for i in range(len(xaxis)):
+        axes2.text(xaxis[i], speed[i], speed[i], size=10)
+
+    fig.tight_layout()
+
+    plt.savefig("/home/student/static/chart.png")
+
+
 def main():
+    """Function main method"""
     minutes = 0
     text_displayed = "Default string"
+    type_speed = []
+    char_acc = []
+    word_acc = []
 
     print("Are you ready to test you tying skills!!!")
-    option = input("Press 'y' to start or 'n' to exit > ")
+    option = input("Press 's' to start or 'e' to exit > ")
 
-    if option == 'y':
-        while option == 'y':
-            os.system('clear')  # Not working in Pycharm, WHY?
-            print("===========================")
-            print("Where you want the text from:")
-            print("1. Random string from file")
-            print("2. Random quote from api call")
-            print("===========================")
-            # print("3. Exit")
-            selection = input("Enter your selection (1 or 2): ")
-            if selection == '1':
-                text_displayed = get_file_string()
-            elif selection == '2':
-                text_displayed = get_api_string()
-            # elif selection == 3:
-            #    break
+    while option.lower() != 's' or option.lower() != 'e':
+        if option.lower() == 's':
+            while option.lower() == 's':
+                os.system('clear')
+                print("===========================")
+                print("Where you want the text from:")
+                print("1. Random string from file")
+                print("2. Random quote from api call")
+                print("===========================")
 
-            print(f"***** {text_displayed} *****")
-            start_time = time.time()
+                selection = input("Enter your selection (1 or 2): ")
+                while selection != '1' or selection != '2':
+                    if selection == '1':
+                        text_displayed = get_file_string()
+                        break
+                    if selection == '2':
+                        text_displayed = get_api_string()
+                        break
+                    else:
+                        print("Invalid selection!!!! PLease enter valid input.")
+                        selection = input("Enter your selection (1 or 2): ")
+                os.system('clear')
+                print(f"***** {text_displayed} *****")
+                print('\n')
 
-            print("Retype the above string inside *** ")
-            text_typed = input(" > ")
+                # Capture typing start time
+                start_time = time.time()
 
-            # Calculating Speed
-            end_time = time.time()
+                print("Retype the above string inside *** ")
+                text_typed = input(" > ")
 
-            time_taken = int(end_time - start_time)
-            if time_taken >= 60:
-                # convert into minutes
-                minutes = int(time_taken / 60)
-                seconds = int(time_taken % 60)
-            else:
-                seconds = time_taken
+                # Capture tying end time
+                end_time = time.time()
 
-            num_of_words = len(text_displayed.split())
-            speed = round((num_of_words / time_taken * 60), 2)
-            char_accuracy = char_accuracy_calc(text_displayed, text_typed)
-            word_accuracy = word_accuracy_calc(text_displayed, text_typed)
+                # Total time taken to type
+                time_taken = int(end_time - start_time)
+                if time_taken >= 60:
+                    # convert into minutes
+                    minutes = int(time_taken / 60)
+                    seconds = int(time_taken % 60)
+                else:
+                    seconds = time_taken
 
-            print("===========================")
-            if time_taken >= 60:
-                print(f"Total Time Taken: {minutes} mins {seconds} secs")
-            else:
-                print(f"Total Time Taken: {seconds} secs")
-            print(f"Your Speed: {speed} wpm")
-            print(f"Accuracy per character: {char_accuracy}%")
-            print(f"Accuracy per word: {word_accuracy}%")
-            print("===========================")
+                num_of_words = len(text_displayed.split())
 
-            print("Do you want to try again?")
-            option = input("Press 'y' to start or 'n' to exit > ")
+                # Typing speed calculation
+                speed = round((num_of_words / time_taken * 60), 2)
 
-    if option == 'n':
-        os.system('clear')
-        print("Goodbye!!!")
+                # Calling function to calculate accuracy
+                char_accuracy = char_accuracy_calc(text_displayed, text_typed)
+                word_accuracy = word_accuracy_calc(text_displayed, text_typed)
+
+                char_acc.insert(len(char_acc), char_accuracy)
+                word_acc.insert(len(word_acc), word_accuracy)
+                type_speed.insert(len(type_speed), speed)
+
+                print("===========================")
+                if time_taken >= 60:
+                    print(f"Total Time Taken: {minutes} mins {seconds} secs")
+                else:
+                    print(f"Total Time Taken: {seconds} secs")
+                print(f"Your Speed: {speed} wpm")
+                print(f"Accuracy per character: {char_accuracy}%")
+                print(f"Accuracy per word: {word_accuracy}%")
+                print("===========================")
+
+                print("Do you want to try again?")
+                option = input("Press 's' to start or 'e' to exit > ")
+
+        elif option.lower() == 'e':
+            chart(char_acc, word_acc, type_speed)
+            print("Goodbye!!!")
+            break
+
+        else:
+            print("Invalid selection!!!! PLease enter valid input.")
+            option = input("Press 's' to start or 'e' to exit > ")
 
 
 if __name__ == "__main__":
